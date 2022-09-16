8d54eb5e250808b707e261b16adaa06dc156774e
ab961 <ab961@MSI>
2022-09-16 17:03:33 +0530

    Add project files.

---
+++ Application/Add.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.IO;
+using System.Text;  
+using System.Threading.Tasks;
+using System.Configuration;
+
+namespace Application
+{
+    public class Add
+    {
+        public static void Execute()
+        {
+            {
+                string desktopPath = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+                string path = desktopPath + @"\file.txt";
+
+                List<User> UserData = ListData.getUserData();
+
+                
+                string[] previousData;
+
+                try
+                {
+                    
+                    File.ReadAllText(path);
+
+                }
+                catch (Exception e)
+                {
+                    File.WriteAllText(path,"");
+
+                }
+                finally
+                {
+
+                }
+
+                string[] UserName = UserData.Select(i => i.UserName.ToString()).ToArray();
+                string[] UserID = UserData.Select(i => i.UserID.ToString()).ToArray();
+                string[] str = new string[1] { " " };
+
+                previousData = File.ReadAllLines(path);
+                bool flag = false;
+
+                for (int i=0;i<UserData.Count;i++)
+                {
+                    File.AppendAllText(path, UserName[i]);
+                    File.AppendAllText(path," ");
+                    File.AppendAllText(path, UserID[i]);
+                    flag = true;
+
+                    if(previousData.Length > 0||flag==true)
+                    {
+                        File.AppendAllLines(path, str);
+                    }
+                }
+
+                previousData = File.ReadAllLines(path);
+
+                Console.WriteLine("List of all users:");
+                foreach (string key in previousData)
+                {
+                    Console.WriteLine($"{key}");
+                }
+                Console.ReadLine();
+
+            }
+        }
+    }
+}
---
+++ Application/ListData.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application
+{
+    internal class ListData
+    {
+        public static List<User> getUserData()
+        {
+            List<User> UserData = new List<User>();
+
+            Console.WriteLine("Enter number of Users you want to add: ");
+            int num;
+            bool check = int.TryParse(Console.ReadLine(), out num);
+
+            for (int i = 1; i <= num; i++)
+            {
+                Console.WriteLine($"Enter Name for DataEntry{i}");
+                string UserName = Console.ReadLine();
+
+                Console.WriteLine($"Enter Id for DataEntry{i}");
+                string userID = Console.ReadLine();
+
+                Console.Write("================================\n");
+
+                User u = new User();
+                u.UserName = UserName;
+                u.UserID = userID;
+
+                UserData.Add(u);
+            }
+            return UserData;
+        }
+    }
+ 
+}
